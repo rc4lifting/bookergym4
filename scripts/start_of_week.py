6d794ffded67df08683cbb0d59b48366db29becf
Benjamin <benjaminlyseow@gmail.com>
2024-07-15 18:26:49 +0800

    changes made on vps

--- scripts/start_of_week.py
+++ scripts/start_of_week.py
@@ -1,5 +1,6 @@
 import os 
 import sys
+import asyncio
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, project_root)
@@ -7,11 +8,11 @@ sys.path.insert(0, project_root)
 from bots.ScheduleBot import ScheduleBot
 from config import logger
 
-def start_week_sheet_automate():
+async def start_week_sheet_automate():
     # delete old sheet
-    ScheduleBot.delete_sheet_old_week()
+    await ScheduleBot.delete_sheet_old_week()
 
 if __name__ == "__main__":
     logger.info("START WEEK AUTOMATION - start")
-    start_week_sheet_automate()
+    asyncio.run(start_week_sheet_automate())
     logger.info("START WEEK AUTOMATION - end")
